edaebfb5c0096b384d83dd6ea8fe323ffc383366
Amber Custer <acuster@madisoncollege.edu>
2021-10-03 18:31:53 -0500

    Create elements.js, select all of the needed elements, and export the elements

---
+++ src/elements.js
@@ -0,0 +1,9 @@
+export const elementSearchButton = document.querySelector('button')
+export const elementSearchedCity = document.querySelector('#city')
+
+export const elementLoadingText = document.querySelector('#load')
+export const elementWeatherBox = document.querySelector('#weather')
+
+export const elementWeatherCityName = elementWeatherBox.firstElementChild
+export const elementWeatherDescription = document.querySelector('#weatherDescription')
+export const elementWeatherTemperature = elementWeatherBox.lastElementChild
