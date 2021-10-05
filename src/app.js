5f759fac958e6a8de3afcafc5247cbeafb5459b8
Amber Custer <acuster@madisoncollege.edu>
2021-10-04 21:45:09 -0500

    Update .gitignore with the properties file, import the apiKey from properties.js, add loading text

--- src/app.js
+++ src/app.js
@@ -8,20 +8,23 @@ import { elementSearchButton,
 } from './elements.js'
 import { Http } from './Http.js'
 import {WeatherData, weatherProxyHandler} from "./WeatherData.js";
+import { apiKey } from './resources/properties.js'
 
-const appID = ''
 const searchWeather = () => {
     const cityName = elementSearchedCity.value.trim()
     if (cityName.length == 0) {
         return alert('Please enter a city name')
     }
-    const url = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid={appID}&units=metric`
+    elementLoadingText.style.display = 'block'
+    elementWeatherBox.style.display = 'none'
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`
 
     Http.fetchData(url)
         .then(responseData => {
             const weatherData = new WeatherData(cityName, responseData.weather[0].description.toUpperCase())
             const weatherProxy = new Proxy(weatherData, weatherProxyHandler)
             weatherProxy.temperature = responseData.main.temp
+            updateWeather(weatherProxy)
         })
         .catch(error => alert(error))
 }
@@ -31,6 +34,7 @@ const updateWeather = weatherData => {
     elementWeatherDescription.textContent = weatherData.description
     elementWeatherTemperature.textContent = weatherData.temperature
 
+    elementLoadingText.style.display = 'none'
     elementWeatherBox.style.display = 'block'
 }
 
