0ce47860212e2dba4f0ffcc660f912e90df93768
Amber Custer <acuster@madisoncollege.edu>
2021-10-04 20:57:42 -0500

    Create a helper module to handle the http request to the api and import it to app.js to handle the data

--- src/app.js
+++ src/app.js
@@ -6,9 +6,21 @@ import { elementSearchButton,
     elementWeatherCityName,
     elementWeatherDescription
 } from './elements.js'
+import { Http } from './Http.js'
 
+const appID = ''
 const searchWeather = () => {
-    alert('clicked')
+    const cityName = elementSearchedCity.value.trim()
+    if (cityName.length == 0) {
+        return alert('Please enter a city name')
+    }
+    const url = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid={appID}&units=metric`
+
+    Http.fetchData(url)
+        .then(responseData => {
+
+        })
+        .catch(error => alert(error))
 }
 
 elementSearchButton.addEventListener('click', searchWeather)
