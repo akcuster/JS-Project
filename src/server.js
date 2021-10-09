d5ba256f91e408c25e92af5c5e44910ddcc048c5
Amber Custer <acuster@madisoncollege.edu>
2021-10-09 17:30:49 -0500

    Uncomment rate limiter code and cors code, update whitelist address

--- src/server.js
+++ src/server.js
@@ -4,16 +4,14 @@ import rateLimit from 'express-rate-limit'
 import cors from 'cors'
 import path, {dirname} from 'path'
 import color from 'chalk'
-import { fileURLToPath} from 'url'
-
+import { fileURLToPath } from 'url'
 import weather from './weather/index.js'
 
 const __dirname = fileURLToPath(dirname(import.meta.url))
 const port = 4000
+const app = express()
+const whitelist = ['http://localhost:4000']
 
-let app = express()
-
-const whitelist = ['http://127.0.0.1']
 const corsOptions = {
     origin: (origin, callback) => {
         if (!origin || whitelist.indexOf(origin) !== -1) {
@@ -25,21 +23,21 @@ const corsOptions = {
     optionsSuccessStatus: 200
 }
 
-app.use(express.json())
-// app.use(cors(corsOptions))
+const limiter = rateLimit({
+    windowMs: 1000,
+    max: 6
+})
 
-// const limiter = rateLimit({
-//     windowMs: 1000,
-//     max: 1
-// })
-// app.use(limiter)
+app.use(express.json())
+app.use(cors(corsOptions))
+app.use(limiter)
 app.use(express.static(path.join(__dirname, 'public')))
-// app.use(express.static(__dirname))
+app.use("/weather", weather)
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "index.html"))
 })
-app.get("/", (req, res) => res.json({success: "hello world!"}))
-app.use("/weather", weather)
+
 
 app.listen(port, () => console.log(`The server is listening on port ${color.green(port)}`))
 
