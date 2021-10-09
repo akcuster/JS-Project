1f3c6ebdd7e8ddb7e48877f0db7ee8b2df477ed4
Amber Custer <acuster@madisoncollege.edu>
2021-10-09 15:21:32 -0500

    Uncomment code to serve up the static files and index.html

--- src/server.js
+++ src/server.js
@@ -26,18 +26,18 @@ const corsOptions = {
 }
 
 app.use(express.json())
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
 
-const limiter = rateLimit({
-    windowMs: 1000,
-    max: 1
-})
-app.use(limiter)
-// app.use(express.static(path.join(__dirname, 'public')))
-// app.use(express.static(__dirname))
-// app.get("/", (req, res) => {
-//     res.sendFile(path.join(__dirname, "views", "index.html"))
+// const limiter = rateLimit({
+//     windowMs: 1000,
+//     max: 1
 // })
+// app.use(limiter)
+app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(__dirname))
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "index.html"))
+})
 app.get("/", (req, res) => res.json({success: "hello world!"}))
 app.use("/weather", weather)
 
